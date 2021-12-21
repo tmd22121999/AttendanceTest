6de68846d5c913196e60008d13ca254875631be2
tmd22121999 <37409790+tmd22121999@users.noreply.github.com>
2021-12-21 17:57:00 +0700

    Update makeup_artist.py

--- makeup_artist.py
+++ makeup_artist.py
@@ -28,69 +28,69 @@ class Makeup_artist(object):
         pass
 
     def apply_makeup(self, img):
-	face_locations = []
-	face_encodings = []
-	face_names = []
-	process_this_frame = True
-	small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-	frame = img
-	# Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-	rgb_small_frame = small_frame[:, :, ::-1]
+        face_locations = []
+        face_encodings = []
+        face_names = []
+        process_this_frame = True
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        frame = img
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
 
-	# Only process every other frame of video to save time
-	if process_this_frame:
-	    # Find all the faces and face encodings in the current frame of video
-	    face_locations = face_recognition.face_locations(rgb_small_frame)
-	    face_encodings = face_recognition.face_encodings(
-		rgb_small_frame, face_locations)
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(
+                rgb_small_frame, face_locations)
 
-	    face_names = []
-	    for face_encoding in face_encodings:
-		# See if the face is a match for the known face(s)
-		matches = face_recognition.compare_faces(
-		    known_face_encodings, face_encoding)
-		name = "Unknown"
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(
+                    known_face_encodings, face_encoding)
+                name = "Unknown"
 
-		# If a match was found in known_face_encodings, just use the first one.
-		# if True in matches:
-		#     first_match_index = matches.index(True)
-		#     name = known_face_names[first_match_index]
+                # If a match was found in known_face_encodings, just use the first one.
+                # if True in matches:
+                #     first_match_index = matches.index(True)
+                #     name = known_face_names[first_match_index]
 
-		# Or instead, use the known face with the smallest distance to the new face
-		face_distances = face_recognition.face_distance(
-		    known_face_encodings, face_encoding)
-		best_match_index = np.argmin(face_distances)
-		if matches[best_match_index]:
-		    name = known_face_names[best_match_index]
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(
+                    known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
 
-		face_names.append(name)
+                face_names.append(name)
 
-	process_this_frame = not process_this_frame
-	# process_this_frame = False
+        process_this_frame = not process_this_frame
+        # process_this_frame = False
 
-	# Add person to attendence set
-	for name in face_names:
-	    attendence_set.add(name)
+        # Add person to attendence set
+        for name in face_names:
+            attendence_set.add(name)
 
-	# Display the results
-	for (top, right, bottom, left), name in zip(face_locations, face_names):
-	    # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-	    top *= 4
-	    right *= 4
-	    bottom *= 4
-	    left *= 4
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
 
-	    # Draw a box around the face
-	    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-	    # Draw a label with a name below the face
-	    cv2.rectangle(frame, (left, bottom - 35),
-			  (right, bottom), (0, 0, 255), cv2.FILLED)
-	    font = cv2.FONT_HERSHEY_DUPLEX
-	    cv2.putText(frame, name, (left + 6, bottom - 6),
-			font, 1.0, (255, 255, 255), 1)
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35),
+                          (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6),
+                        font, 1.0, (255, 255, 255), 1)
 
-	# Display the resulting image
-	ret, buffer = cv2.imencode('.jpg', frame)
-	frame = buffer.tobytes()
-	return buffer
+        # Display the resulting image
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        return buffer
