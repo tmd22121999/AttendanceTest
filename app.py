2099b949033c2e742e17bb37d665911fd8740e6a
tmd22121999 <37409790+tmd22121999@users.noreply.github.com>
2021-12-22 12:46:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import cv2
 from PIL import Image 
 import binascii
 import base64
-
+import face_recognition
 from io import BytesIO
 
 app = Flask(__name__)
@@ -19,14 +19,29 @@ app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
 socketio = SocketIO(app)
 camera = Camera(Makeup_artist())
+known_face_encodings = []
+known_face_names = []
 
+print("Encoding face...")
+FACE_IMG_DIR = os.path.join(os.getcwd(), 'face_img')
+for filename in os.listdir(FACE_IMG_DIR):
+    face = face_recognition.load_image_file(
+        os.path.join(FACE_IMG_DIR, filename))
+    face_encoding = face_recognition.face_encodings(face)[0]
+    known_face_encodings.append(face_encoding)
+    known_face_names.append(re.sub('.jpg$', '', filename))
+print("Encoding face done")
+print(known_face_names)
+
+# Prepare attendece set which contains the names of those who attended
+attendence_set = set()
 
 @socketio.on('input image', namespace='/test')
 def test_message(input):
     input = input.split(",")[1]
     camera.enqueue_input(input)
     image_data = input
-    image_data = processsss(input) # Do your magical Image processing here!!
+    image_data = recog(input) # Do your magical Image processing here!!
     buf = BytesIO()
     image_data.save(buf, format="JPEG")
     image_data = base64.b64encode(buf.getvalue())
@@ -34,7 +49,79 @@ def test_message(input):
     image_data = "data:image/jpeg;base64," + image_data
     #print("OUTPUT " + image_data)
     emit('out-image-event', {'image_data': image_data}, namespace='/test')
+    
+    
+    
+def recog(img):
+        frame =  np.array(img.convert('RGB') )
+        face_locations = []
+        face_encodings = []
+        face_names = []
+        process_this_frame = True
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
 
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(
+                rgb_small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(
+                    known_face_encodings, face_encoding)
+                name = "Unknown"
+
+                # If a match was found in known_face_encodings, just use the first one.
+                # if True in matches:
+                #     first_match_index = matches.index(True)
+                #     name = known_face_names[first_match_index]
+
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(
+                    known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+
+                face_names.append(name)
+                print(face_names)
+
+        process_this_frame = not process_this_frame
+        # process_this_frame = False
+
+        # Add person to attendence set
+        for name in face_names:
+            attendence_set.add(name)
+
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35),
+                          (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6),
+                        font, 1.0, (255, 255, 255), 1)
+        cv2.putText(frame, "wnnvownoveusjwovnwweqweqwe", (25 + 6, 40 - 6),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 1)
+        # Display the resulting image
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = Image.fromarray(frame)
+        return frame.transpose(Image.FLIP_LEFT_RIGHT)
+    
+    
 def processsss(input2):
     img2 = base64_to_pil_image(input2)
     img2 = np.array(img2)
