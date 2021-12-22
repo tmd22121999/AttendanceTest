30f3c8d458bf1dc89097f1d4009f35b8130ea86e
tmd22121999 <37409790+tmd22121999@users.noreply.github.com>
2021-12-22 11:46:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from camera import Camera
 from utils import base64_to_pil_image, pil_image_to_base64
 import numpy as np
 import cv2
+from PIL import Image 
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(stdout))
